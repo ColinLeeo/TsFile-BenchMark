ca4c4a4c59ed508300ee449d9735908699749aed
ColinLee <shuolin_l@163.com>
2025-07-24 01:23:09 +0800

    fix scripts.

--- scripts/start_benchmark_docker.sh
+++ scripts/start_benchmark_docker.sh
@@ -2,21 +2,20 @@
 
 set -e
 
-if [-d "tsfile"]; then
+if [ -d "tsfile" ]; then
     rm -rf tsfile
     echo "TsFile directory found. Remove it"
 fi
 
 # Clone the TsFile repository
-git clone https://github.com/apache/tsfile.git
+git clone git@github.com:ColinLeeo/tsfile.git
 
 docker run --rm --privileged \
     -v "$(pwd)/tsfile:/workspace/tsfile" \
     -v "/home/colin/dev/TsFile-BenchMark/benchmark_core:/workspace/benchmark_core" \
     -v "$(pwd)/result:/result" \
-    -v "/home/colin/dev/TsFile-BenchMark/benchmark_core/config:/tmp/config" \
-    -v "/home/colin/dev/FlameGraph:/workspace/FlameGraph" \
-    -v "/home/colin/dev/TsFile-BenchMark/docker/run_benchmark.sh:/workspace/run_benchmark.sh" \
+    -v "$(pwd)/../docker/run_benchmark.sh:/workspace/run_benchmark.sh" \
+    -v "$(pwd)/../benchmark_core/config:/tmp/config" \
     -w /workspace \
-    tsfile-benchmark:latest \
+    tsfile_benchmark:latest \
     bash -c "chmod +x run_benchmark.sh && ./run_benchmark.sh"
