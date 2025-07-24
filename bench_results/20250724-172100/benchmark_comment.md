cc5d9056be86a89e424d1c2cb61b1ed51bbc5d2a
colin <shuolin_l@163.com>
2025-07-24 17:25:05 +0800

    add some scrpits.
    add gitignore.
    
    benchmark result 20250724-172833
    
    fix py.

---
+++ bench_results/20250724-172100/benchmark_comment.md
@@ -0,0 +1,7 @@
+## Benchmark result of tsfile at 20250724-172100 - commit
+### write and read result
+| language | tsfile_size | prepare_time | writing_time | writing_speed | reading_time | reading_speed |
+|----------|-------------|--------------|--------------|---------------|--------------|---------------|
+| Java | 4420 | 5.087850913 | 17.01619533 | 11310146.443399295 | 11.655581713 | 30028531 |
+| Cpp | 4630 | 20.044588 | 12.729640999999999 | 7627944.0 | 20.4031 | 12253039 |
+
--- scripts/process_data_and_report_benchmark.py
+++ scripts/process_data_and_report_benchmark.py
@@ -1,24 +1,32 @@
 import json
 import os
+import shutil
 from datetime import datetime
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import requests
+from git import Repo
 
 from docutils.nodes import row
 
+## 1. Config info
 LANGUAGES = ["java", "cpp", "python"]
 BASE_DIR = "result"
 OUTPUT_DIR = "generated_data"
 COMMENTED_MD = os.path.join(OUTPUT_DIR, "benchmark_comment.md")
 IMAGE_FILE = os.path.join(OUTPUT_DIR, "memory_usage.png")
-
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+BASE_UPLOAD_DIR = "../bench_results"
+GITHUB_TOKEN = ""
 REPO = "ColinLeeo/tsfile"
-ISSUE_NUMBER = "460"
+ISSUE_NUMBER = "2"
+PIC_REPO = "ColinLeeo/TsFile-BenchMark"
+ISSUE_NUMBER_PIC = "1"
 
+## 2. Result will be stored at output dir.
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
+## 3. Process data and generate a markdown + png.
 benchmark_data = []
 for language in LANGUAGES:
     json_path = os.path.join(BASE_DIR, f"results_{language}.json")
@@ -31,25 +39,32 @@ for language in LANGUAGES:
             [language.capitalize(),
              data.get("tsfile_size", ""),
              data.get("prepare_time", ""),
-             data.get("writing_time", ""),
+             data.get("write_time", ""),
              data.get("writing_speed", ""),
              data.get("reading_time", ""),
              data.get("reading_speed", "")]
         )
 
 
-
-table_header = "| language | tsfile_size | prepare_time | writing_time | writing_speed | reading_time | reading_speed |"
-table_div    = "|----------|-------------|--------------|--------------|---------------|--------------|---------------|"
+table_header = "| language | tsfile_size | prepare_time | writing_time | writing_speed | reading_time | reading_speed |\n"
+table_div    = "|----------|-------------|--------------|--------------|---------------|--------------|---------------|\n"
 table_rows = [
-    f"| {row[0]} | {row[1]} | {row[2]} | {row[3]} | {row[4]} | {row[5]} |"
+    f"| {row[0]} | {row[1]} | {row[2]} | {row[3]} | {row[4]} | {row[5]} | {row[6]} |"
     for row in benchmark_data
 ]
+now_str = datetime.now().strftime("%Y%m%d-%H%M%S")
+with open(COMMENTED_MD, "w") as f:
+    f.write(f"## Benchmark result of tsfile at {now_str} - commit\n")
+    f.write("### write and read result\n")
+    f.write(table_header)
+    f.write(table_div)
+    f.write("\n".join(table_rows))
+    f.write("\n\n")
 
 plt.figure(figsize = (10, 6))
 
 for lang in LANGUAGES:
-    csv = os.path.join(OUTPUT_DIR, "memory_usage_{lang}.csv")
+    csv = os.path.join(BASE_DIR, f"memory_usage_{lang}.csv")
     if not os.path.exists(csv):
         print(f"Couldn't find {csv}")
         continue
@@ -63,13 +78,47 @@ plt.legend()
 plt.tight_layout()
 plt.savefig(IMAGE_FILE)
 
-now_str = datetime.now().strftime("%Y%m%d-%H%M%S")
-with open(COMMENTED_MD, "w") as f:
-    f.write(f"## Benchmark result of tsfile at {now_str} - commit\n")
-    f.write("write and read result\n")
-    f.write(table_header)
-    f.write(table_div)
-    f.write("\n".join(table_rows))
-    f.write("\n")
-    f.write("memory usage of tsfile when writing data\n")
+## commit this results.
+repo = Repo("..")
+COMMIT_OUTPUT_DIR = os.path.join(repo.working_dir, "bench_results", now_str)
+os.makedirs(COMMIT_OUTPUT_DIR, exist_ok=True)
+shutil.copy(IMAGE_FILE, COMMIT_OUTPUT_DIR)
+shutil.copy(COMMENTED_MD, COMMIT_OUTPUT_DIR)
 
+repo.git.add(COMMIT_OUTPUT_DIR)
+repo.index.commit(f"benchmark result {now_str}")
+origin = repo.remote(name="origin")
+origin.push()
+
+
+## Update markdown files.
+image_url =  f"https://raw.githubusercontent.com/{PIC_REPO}/main/bench_results/{now_str}/memory_usage.png"
+with open(COMMENTED_MD, "a") as f:
+    f.write("#### memory usage of tsfile when writing data\n")
+    f.write(f"![memory usage]({image_url})\n")
+
+
+headers = {
+    "Authorization": f"token {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github+json"
+}
+
+with open(COMMENTED_MD, "r") as f:
+    comment_body = f.read()
+
+response = requests.post(
+    f"https://api.github.com/repos/{REPO}/issues/{ISSUE_NUMBER}/comments",
+    headers=headers,
+    json={"body": comment_body}
+)
+
+response.raise_for_status()
+
+patch_resp = requests.patch(
+    f"https://api.github.com/repos/{REPO}/issues/{ISSUE_NUMBER}",
+    headers=headers,
+    json={"body": comment_body}
+)
+
+patch_resp.raise_for_status()
+print("✅ Issue body updated with latest timestamp.")
